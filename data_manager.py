79fa0766ff8cddf8701f3d86a4d1ca77ef451b6d
Lilla Meszaros <ameszaroslilla@gmail.com>
2022-07-29 10:16:05 +0200

    order by +id added

--- data_manager.py
+++ data_manager.py
@@ -10,7 +10,7 @@ def sort_questions(order_by: str, order_direction: str) -> None:
     :param order_by: title, submission_time, message, view_number, vote_number
     :param order_direction: asc, desc
     '''
-    if order_by in ['title', 'submission_time', 'message', 'view_number', 'vote_number'] \
+    if order_by in ['id', 'title', 'submission_time', 'message', 'view_number', 'vote_number'] \
             and order_direction in ['asc', 'desc']:
         questions = get_questions("./sample_data/question.csv")
         sorted_questions = sorted(questions, key=lambda question: question[order_by], reverse=order_direction == "desc")
@@ -24,7 +24,7 @@ def get_question_by_id(question_id: int) -> dict:
             return question
 
 
-def get_answers_by_question_id(question_id: int) -> list[dict]:
+def get_answers_by_question_id(question_id: int):
     answers_list = []
     answers = get_answers("./sample_data/answer.csv")
     for i, answer in enumerate(answers):
