422794577aa2e1fadd57264a3e98e296e6c694f4
Budzserator <borokbora@gmail.com>
2022-08-10 12:58:17 +0200

    Resolve conflict after merging

--- server.py
+++ server.py
@@ -3,8 +3,7 @@ from connection import write_question_and_return_new_id, write_answer, del_answe
     update_question_by_id, update_answer_by_id, write_comment_by_answer_id, write_comment_to_comment
 
 from data_manager import get_sorted_questions, get_question_by_id, get_answers_by_question_id, get_answer_by_id, \
-    get_question_id_by_answer_id, get_comments
-    get_questions, get_latest_questions
+    get_question_id_by_answer_id, get_comments, get_questions, get_latest_questions
 
 
 import os
