b26d5efbf961cc6b2e3cdc567da925f10b30fac9
Lilla Meszaros <ameszaroslilla@gmail.com>
2022-08-11 08:17:27 +0200

    delete comment

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template, redirect
 from connection import write_question_and_return_new_id, write_answer, del_answer_by_id, del_question_by_id, \
-    update_question_by_id, update_answer_by_id, write_comment_by_answer_id, update_comment_by_id, update_comment_edit, update_comment_submission_time
+    update_question_by_id, update_answer_by_id, write_comment_by_answer_id, update_comment_by_id, update_comment_edit, \
+    update_comment_submission_time, attach_tags, del_tag_by_question_id, write_comment_to_comment, \
+    delete_comment_by_id
 
 from data_manager import get_sorted_questions, get_question_by_id, get_answers_by_question_id, get_answer_by_id, \
     get_question_id_by_answer_id, get_comments, get_answer_id_from_comment, get_comment_by_id, \
-    get_edit_count_by_comment_id
+    get_edit_count_by_comment_id, get_tags_by_question_id, get_tags, get_questions, get_latest_questions, \
+    get_search_question, get_search_answer
+
 import os
 from werkzeug.utils import secure_filename
 
@@ -24,8 +28,9 @@ def allowed_file(filename):
 
 
 @app.route('/')
-def starting_page():
-    return list()
+def index():
+    questions = get_latest_questions()
+    return render_template('index.html', questions=questions)
 
 
 @app.route('/list', methods=['GET', 'POST'])
@@ -51,24 +56,50 @@ def get_question(question_id):
     update_question_by_id(question_id, question)
     answers = get_answers_by_question_id(question_id)
     comments = get_comments()
-    return render_template("questions.html", question=question, answers=answers, comments=comments)
+    tags = get_tags_by_question_id(question_id)
+    return render_template("questions.html", question=question, answers=answers, tags=tags, comments=comments)
+
+
+@app.route("/search", methods=['POST'])
+def get_search_result():
+    if request.method == 'POST':
+        search_phrase = request.form["search-question"]
+        searched_question = get_search_question(search_phrase)
+        searched_answer = get_search_answer(search_phrase)
+        return render_template("list.html", questions=searched_question, searched_answers=searched_answer)
 
 
 @app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
 def edit_question(question_id):
     if request.method == 'POST':
         question = request.form.to_dict()
+        tags = None
+        if "tags" in question:
+            question.pop("tags")
+            tags = request.form.getlist("tags")
         update_question_by_id(question_id, question)
+        if tags != None:
+            tags = [{"question_id": question_id, "tag_id": tag_id} for tag_id in tags]
+            attach_tags(tags)
+        else:
+            del_tag_by_question_id(question_id)
         return redirect(f"/question/{question_id}")
     else:
         question = get_question_by_id(int(question_id))
-        return render_template('add-question.html', question=question)
+        ids_of_selected_tags = [tag["id"] for tag in get_tags_by_question_id(question_id)]
+        all_tags = get_tags()
+        return render_template('add-question.html', question=question, all_tags=all_tags,
+                               ids_of_selected_tags=ids_of_selected_tags)
 
 
 @app.route('/add-question', methods=["GET", "POST"])
 def add_question():
     if request.method == "POST":
         new_question = request.form.to_dict()
+        tags = None
+        if "tags" in new_question:
+            new_question.pop("tags")
+            tags = request.form.getlist("tags")
         # check if the post request has the file part
         # if 'image' not in request.files:
         #     flash('No file part')
@@ -84,8 +115,12 @@ def add_question():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             new_question["image"] = str(os.path.join(app.config['UPLOAD_FOLDER'], filename))[1:]
         question_id = write_question_and_return_new_id(new_question)
-        return redirect(f'/question/{question_id}')
-    return render_template('add-question.html', question={})
+        if tags != None:
+            tags = [{"question_id": question_id, "tag_id": tag_id} for tag_id in tags]
+            attach_tags(tags)
+            return redirect(f'/question/{question_id}')
+    all_tags = get_tags()
+    return render_template('add-question.html', question={}, all_tags=all_tags)
 
 
 @app.route('/question/<question_id>/new-answer', methods=["GET", "POST"])
@@ -123,6 +158,19 @@ def add_a_comment_to_answer(answer_id):
         return redirect(f"/question/{question_id}")
 
 
+@app.route('/answer/<answer_id>/<parent_comment_id>', methods=['POST', 'GET'])
+def add_a_comment_to_comment(answer_id, parent_comment_id):
+    if request.method == 'GET':
+        return render_template('comment-to-comment.html')
+    elif request.method == 'POST':
+        new_comment = request.form["comment-to-comment"]
+        write_comment_to_comment(parent_comment_id, answer_id, new_comment)
+        question_id = get_question_id_by_answer_id(answer_id)
+        return redirect(f"/question/{question_id}")
+        question_id = get_question_id_by_answer_id(answer_id)
+        return redirect(f"/question/{question_id}")
+
+
 @app.route('/question/<question_id>/delete')
 def delete_question_id(question_id):
     question_id = int(question_id)
@@ -142,7 +190,7 @@ def question_vote_up(question_id):
     question = get_question_by_id(question_id)
     question["vote_count"] += 1
     update_question_by_id(question_id, question)
-    return redirect("/list")
+    return redirect(request.referrer)
 
 
 @app.route('/question/<question_id>/vote-down')
@@ -150,7 +198,7 @@ def question_vote_down(question_id):
     question = get_question_by_id(question_id)
     question["vote_count"] -= 1
     update_question_by_id(question_id, question)
-    return redirect("/list")
+    return redirect(request.referrer)
 
 
 @app.route('/answer/<answer_id>/vote-up')
@@ -160,7 +208,7 @@ def answer_vote_up(answer_id):
     answer = get_answer_by_id(answer_id)
     answer["vote_count"] += 1
     update_answer_by_id(answer_id, answer)
-    return redirect(f"/question/{question_id}")
+    return redirect(request.referrer)
 
 
 @app.route('/answer/<answer_id>/vote-down')
@@ -170,7 +218,7 @@ def answer_vote_down(answer_id):
     answer = get_answer_by_id(answer_id)
     answer["vote_count"] -= 1
     update_answer_by_id(answer_id, answer)
-    return redirect(f"/question/{question_id}")
+    return redirect(request.referrer)
 
 
 # edit answer:
@@ -204,6 +252,13 @@ def edit_comment(comment_id):
         comment = get_comment_by_id(comment_id)
         answer_id = get_answer_id_from_comment(comment_id)
         return render_template('update-comment.html', comment=comment, comment_id=comment_id, answer_id=answer_id)
+
+
+# delete comment
+@app.route('/comments/<comment_id>/delete')
+def delete_comment(comment_id):
+    delete_comment_by_id(comment_id)
+    return redirect(request.referrer)
 
 
 if __name__ == "__main__":
